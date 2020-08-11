6c5f1a655014d71062fc0b5979104d7b98eb8a83
Gary Lo <logap@me.com>
2020-08-12 01:56:11 +0800

    support nerd font #1

--- .travis/commit.sh
+++ .travis/commit.sh
@@ -3,6 +3,7 @@
 FIRA_TAG=$(cd fonts/fira && git describe --tags `git rev-list --tags --max-count=1`);
 HACK_TAG=$(cd fonts/hack && git describe --tags `git rev-list --tags --max-count=1`);
 JETBRAINS_TAG=$(cd fonts/jetbrainsmono && git describe --tags `git rev-list --tags --max-count=1`);
+HACK_NERD_TAG=$(curl https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r .tag_name);
 
 git config --global user.email "logap@me.com";
 git config --global user.name "Gary Lo";
@@ -12,9 +13,9 @@ if output=$(git status --porcelain) && [ -z "$output" ]; then
   exit 0;
 else
   # Uncommitted changes
-  GIT_TAG="${HACK_TAG}+FC${FIRA_TAG}+JBM${JETBRAINS_TAG}"
+  GIT_TAG="${HACK_TAG}+N${HACK_NERD_TAG}+FC${FIRA_TAG}+JBM${JETBRAINS_TAG}"
   git add .
-  git commit -m "Update font release Hack@${HACK_TAG}, FiraCode@${FIRA_TAG}, JetBrainsMono@${JETBRAINS_TAG}";
+  git commit -m "Update font release Hack@${HACK_TAG}, HackNerd@${HACK_NERD_TAG} FiraCode@${FIRA_TAG}, JetBrainsMono@${JETBRAINS_TAG}";
   git tag "$GIT_TAG";
 
   # Remove existing "origin"
